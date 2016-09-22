380d5f810479503e7395831736f70ada1d644333
郑付超 <fuchao.zheng@ikang.com>
2016-09-22 14:58:05 +0800

    use epi to package and distribute app

--- app/app.ts
+++ app/app.ts
@@ -19,20 +19,20 @@ class ByteFormatPipe implements PipeTransform {
   pipes: [ByteFormatPipe],
   template: `
 
-    <h1>Total Images: {{ imageStats().count }}</h1>
-    <h1>Total Size: {{ imageStats().size | byteFormat}}</h1>
+    <h1>总共: {{ imageStats().count }}</h1>
+    <h1>大小: {{ imageStats().size | byteFormat}}</h1>
 
     <div
       (dragover)="false"
       (dragend)="false"
       (drop)="handleDrop($event)"
-      style="height: 300px; border: 5px dotted #ccc;">
+      style="height: 100px; border: 5px dotted #ccc;">
       <p style="margin: 10px; text-align: center">
-        <strong>Drop Your Images Here</strong>
+        <strong>把图片拖进来看看效果</strong>
       </p>
     </div>
 
-    <div class="media" *ngFor="#image of images">
+    <div class="media" *ngFor="let image of images">
       <div class="media-left">
         <a href="#">
           <img class="media-object" src="{{image.path}}" style="max-width:200px">
@@ -63,7 +63,7 @@ export class App {
         alert("File must be a PNG or JPEG!");
       }
     });
-
+    console.log(e);
     return false;
   }
 
